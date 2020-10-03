929b63d995ff06c59d21ee74c9157567f08b78b6
Alan Fernandes <anoblefernandes@gmail.com>
2020-10-03 14:39:57 -0700

    Uploading Files for Burger App

---
+++ Develop/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Bacon');
+INSERT INTO burger (name) VALUES ('Bacon Cheese');
+INSERT INTO burger (name, devour) VALUES ('Bacon Jalepino', true);
+INSERT INTO burger (name, devour) VALUES ('Plain Burger', true);
+INSERT INTO burger (name, devour) VALUES ('Double Cheese', true);
+INSERT INTO burger (name) VALUES ('Double Bacon Cheese');
